6e1a27b29e2b592b720d41ca83b21dffa1e54685
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-29 09:18:45 -0300

    configured http404 exception

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path("<int:question_id>/", views.detail, name="detail"),
 
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/results/", views.results, name="results"),
 
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
