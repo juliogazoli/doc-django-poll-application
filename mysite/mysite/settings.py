23e031f5e30eaca01b6502d89627d83d9ba45b22
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-12-06 07:52:00 -0300

    installed django debug toolbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,18 +30,20 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    
+INSTALLED_APPS = [  
     'django.contrib.admin', # The admin site
     'django.contrib.auth', # An authentication system.
     'django.contrib.contenttypes', # A framework for content types.
     'django.contrib.sessions', # A session framework.
     'django.contrib.messages', # A messaging framework.
     'django.contrib.staticfiles', # A framework for managing static files.
+    
+    'debug_toolbar',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
